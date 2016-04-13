d9d515abfc225e84c5675e9ae45bfcebae5946d1
Dharnu <dharu7753@gmail.com>
2016-04-13 17:56:15 +0530

    Json object for main View and graph

--- js/project1.js
+++ js/project1.js
@@ -1,16 +1,32 @@
 (function(){
 	var app=angular.module('project1',['project1-directives']);
 	app.controller('Maincontroller',function(){
-		this.emergencyListItems=[{
-			id:1, data:"data1"
+            this.emergencyListItems=[{
+                id:1, data:"data1"
+                },{
+                id:2, data:"data2"
+                },{
+                id:3, data:"data3"
+                },{
+                id:4, data:"data4"
+            }];
+            this.mainView=[{
+                id:1, data:{"Patient":"Jane","Office":"Lafayette","Phone":"555-555-5555","POC":"Employee 3","LastActivity":"10:00 AM"}
 		},{
-			id:2, data:"data2"
+                id:2, data:{"Patient":"John","Office":"Columbus","Phone":"555-555-5500","POC":"Employee 1","LastActivity":"10:00 AM"}
 		},{
-			id:3, data:"data3"
+                id:3, data:{"Patient":"Emma","Office":"Lafayette","Phone":"555-555-5552","POC":"Employee 3","LastActivity":"8:00 AM"}
 		},{
-			id:4, data:"data4"
-		}];
-
-
+                id:4, data:{"Patient":"Ed","Office":"Lafayette","Phone":"555-555-5533","POC":"Employee 3","LastActivity":"No Activity"}
+            }];
+            this.graphImage=[{
+                id:1, image:"images/chart1.png"
+                },{
+                id:2, image:"images/chart2.png"
+                },{
+                id:3, image:"images/chart3.png"
+                },{
+                id:4, image:"images/chart4.jpg"
+            }];
 	});
 })();
--- js/project1directives.js
+++ js/project1directives.js
@@ -1,41 +1,57 @@
 (function(){
-var app=angular.module('project1-directives',[]);
+    var app=angular.module('project1-directives',[]);
 
-app.directive('emergencyList',function(){
+    app.directive('emergencyList',function(){
 	return{
-		restrict:'E',
-		scope:{},
-		templateUrl:"./partials/emergency-list.html",
-		require:['emergencyList'],
-		controller:function(){
-			this.alertObjects},
-		controllerAs:'emergencyList',
-		link:function(s,e,a,c){
-			c[0].alertObjects=s.$parent.$parent.main.emergencyListItems;
-			console.log(c[0]);
-		}
-	}
-	});
+            restrict:'E',
+            scope:{},
+            templateUrl:"./partials/emergency-list.html",
+            require:['emergencyList'],
+            controller:function(){
+                    this.alertObjects},
+            controllerAs:'emergencyList',
+            link:function(s,e,a,c){
+                    c[0].alertObjects=s.$parent.$parent.main.emergencyListItems;
+                    console.log(c[0]);
+            }
+        }
+    });
 
-app.directive("pageComponents",function(){
+    app.directive("pageComponents",function(){
 	return{
-		restrict:'E',
-		scope:{},
-		templateUrl:"./partials/page-components.html",
-		controller:function(){},
-		link:function(s,e,a,c){
-			console.log(s.$parent.main);
-		}
+            restrict:'E',
+            scope:{},
+            templateUrl:"./partials/page-components.html",
+            controller:function(){},
+            link:function(s,e,a,c){
+                    console.log(s.$parent.main);
+            }
 	}
-	});
+    });
 
-app.directive("graph",function(){
+    app.directive("graph",function(){
 	return{
-		restrict:'E',
-		scope:{},
-		templateUrl:"./partials/graph.html",
-		controller:function(){},
-		link:function(s,e,a,c){}
+            restrict:'E',
+            scope:{},
+            templateUrl:"./partials/graph.html",
+            controller:function(){},
+            link:function(s,e,a,c){}
 	}
-	});
-	})();
+    });
+        
+    app.directive('mainView',function(){
+        return{
+            restrict:'E',
+            scope:{},
+            templateUrl:"./partials/main-view.html",
+            require:['mainView'],
+            controller:function(){
+                    this.individualValue},
+            controllerAs:'mainView',
+            link:function(s,e,a,c){
+                    c[0].individualValue=s.$parent.$parent.main.mainView;
+                    console.log(c[0]);
+            }
+        }
+    });
+})();
