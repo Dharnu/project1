de5272aa409060445c8cad79d47c00a873f788c8
Hemant <HemantP@e2Infos-Mac-mini.local>
2016-04-15 09:47:46 +0530

    comminicatiojn complete push new elemnt to top animations to be done

--- js/project1directives.js
+++ js/project1directives.js
@@ -1,86 +1,94 @@
-(function(){
-    var app=angular.module('project1-directives',[]);
+(function() {
+	var app = angular.module('project1-directives', []);
 
 
-app.directive('emergencyList',['communication',function(communication){
-	return{
-		restrict:'E',
-		scope:{},
-		templateUrl:"./partials/emergency-list.html",
-		require:['emergencyList','^pageComponents'],
-		controller:function($rootScope, $scope){
-			this.alertObjects;
-			this.click=function(num){
-				//console.log(this.alertObjects[num].id);
-				communication.tellgraph(this.alertObjects[num].id);
+	app.directive('emergencyList', ['communication', function(communication) {
+		return {
+			restrict: 'E',
+			scope: {},
+			templateUrl: "./partials/emergency-list.html",
+			require: ['emergencyList', '^pageComponents'],
+			controller: function($rootScope, $scope) {
+				this.alertObjects;
+				this.click = function(num) {
+					//console.log(this.alertObjects[num].id);
+					communication.tellgraph(this.alertObjects[num].id);
 				};
-                                this.addEvent = function(){
-                                    console.log("add event");
-                                    this.alertObjects.push({
-                                        id:10, data: $scope.createEvent
-                                    });
-                                };
+				this.addEvent = function() {
+					console.log("add event");
+					this.alertObjects.unshift({
+						id: 10,
+						data: $scope.createEvent
+					});
+				};
 			},
-		controllerAs:'emergencyList',
-		link:function(s,e,a,c){
-			c[0].alertObjects=s.$parent.$parent.main.emergencyListItems;
-			console.log(c[1]);
+			controllerAs: 'emergencyList',
+			link: function(s, e, a, c) {
+				c[0].alertObjects = s.$parent.$parent.main.emergencyListItems;
+				console.log(c[1]);
+			}
 		}
-	}
 	}]);
 
-    app.directive("pageComponents",function(){
-	return{
-		restrict:'E',
-		scope:{},
-		templateUrl:"./partials/page-components.html",
-		controller:function(){},
-		link:function(s,e,a,c){
-		//	console.log(s.$parent.main);
+	app.directive("pageComponents", function() {
+		return {
+			restrict: 'E',
+			scope: {},
+			templateUrl: "./partials/page-components.html",
+			controller: function() {},
+			link: function(s, e, a, c) {
+				//	console.log(s.$parent.main);
+			}
 		}
-	}
-    });
+	});
 
-    app.directive("graph",['communication',function(communication){
-	return{
-		restrict:'E',
-		scope:{},
-		require:['graph'],
-		templateUrl:"./partials/graph.html",
-		controller:function(){
-			this.graphs;
-			this.displayImage
-		},
-		controllerAs:"graphImage",
-		link:function(s,e,a,c){
-		c[0].graphs=s.$parent.$parent.main.graphImages;
-		c[0].displayImage=c[0].graphs.id1;
-			s.$on("graph",function(){
-				console.log("recieved broadcast");
-				//console.log(communication.id);
-				var id = "id"+communication.id;
-				//console.log(c[0].graphs);
-				c[0].displayImage=c[0].graphs.id;
+	app.directive("graph", ['communication', function(communication) {
+		return {
+			restrict: 'E',
+			scope: {},
+			require: ['graph'],
+			templateUrl: "./partials/graph.html",
+			controller: function() {
+				this.graphs;
+				this.displayImage
+			},
+			controllerAs: "graphImage",
+			link: function(s, e, a, c) {
+				c[0].graphs = s.$parent.$parent.main.graphImages;
+				c[0].displayImage = c[0].graphs.id1;
+				s.$on("graph", function() {
+					console.log("recieved broadcast");
+					//console.log(communication.id);
+					var idxx = "id" + communication.id;
+					console.log(idxx);
+					//console.log(c[0].graphs);
+					//console.log(c[0].graphs[idxx]);
+					c[0].displayImage = c[0].graphs[idxx];
 
-			});
+				});
+			}
 		}
-	}
-    }]);
-        
-    app.directive('mainView',function(){
-        return{
-            restrict:'E',
-            scope:{},
-            templateUrl:"./partials/main-view.html",
-            require:['mainView'],
-            controller:function(){
-                    this.individualValue},
-            controllerAs:'mainView',
-            link:function(s,e,a,c){
-                    c[0].individualValue=s.$parent.$parent.main.mainView;
-            }
-        }
-    });
-    
-    
+	}]);
+
+	app.directive('mainView', ['communication', function(communication) {
+		return {
+			restrict: 'E',
+			scope: {},
+			templateUrl: "./partials/main-view.html",
+			require: ['mainView'],
+			controller: function() {
+				this.individualValue;
+				this.click = function(num) {
+					console.log("mainview click");
+					communication.tellgraph(this.individualValue[num].id);
+				};
+			},
+			controllerAs: 'mainView',
+			link: function(s, e, a, c) {
+				c[0].individualValue = s.$parent.$parent.main.mainView;
+			}
+		}
+	}]);
+
+
 })();
--- js/project1factories.js
+++ js/project1factories.js
@@ -1,8 +1,7 @@
 (function(){
 	var app=angular.module('project1-factories',[]);
 	app.factory('communication',function($rootScope){
-		var obj={
-			id:0
+		var obj={id:0
 		};
 		obj.tellgraph=function(id){
 			this.id=id;
