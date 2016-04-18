3a76d758df3f37c3a5122daafa262832418be9d1
Hemant <HemantP@muralie.e2info.com>
2016-04-18 17:21:23 +0530

    unable to fix pause

--- js/project1directives.js
+++ js/project1directives.js
@@ -1,15 +1,15 @@
 (function() {
 	var app = angular.module('project1-directives', ['ngAnimate']);
 
-	app.filter('stopdisplay',[function(){
-		return function(alerts){
-			var toDisplayAlerts=[];
-		//	console.log(alerts);
-			for(i=0;i<alerts.length;i++){
-			//	console.log(alerts[i].displayed);
-			if(alerts[i].displayed)
-				toDisplayAlerts.push(alerts[i]);
-		};
+	app.filter('stopdisplay', [function() {
+		return function(alerts) {
+			var toDisplayAlerts = [];
+			//	console.log(alerts);
+			for (i = 0; i < alerts.length; i++) {
+				//	console.log(alerts[i].displayed);
+				if (alerts[i].displayed)
+					toDisplayAlerts.push(alerts[i]);
+			};
 			return toDisplayAlerts;
 		}
 	}]);
@@ -26,7 +26,7 @@
 			controller: function($rootScope, $scope) {
 				//this.alertObjects=$scope.$parent.$parent.main.emergencyListItems;				
 				//this.addElement=function
-				this.interval=$interval;
+				this.interval = $interval;
 				this.click = function(num) {
 					communication.tellgraph(this.alertObjects[num].id);
 				};
@@ -42,47 +42,81 @@
 			link: function(s, e, a, c) {
 				console.log(a);
 				c[0].alertObjects = s.$parent.$parent.main.emergencyListItems;
-				for(i=0;i<c[0].alertObjects.length;i++){
+				for (i = 0; i < c[0].alertObjects.length; i++) {
 					//console.log(c[0].alertObjects[i]);
-					c[0].alertObjects[i]["displayed"]=true;
-				//	c[0].alertObjects[i]["uniqueId"]=new Date().getTime();
+					c[0].alertObjects[i]["displayed"] = true;
+					//	c[0].alertObjects[i]["uniqueId"]=new Date().getTime();
 
 				};
 				console.log(c[0].alertObjects);
 				c[0].addElementForDemo = (function() {
 					var copiedIndex = 0;
+					var objectsToSpliceFromBottom = 1;
 					return function(paused) {
-						c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex]["displayed"]=!paused;
-						// c[0].alertObjects.unshift(
-						// 	{
-						// 		id:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].id,
-						// 		data:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].data
-						// 	}
-						// 	);
-						//hack against deep copying since it was notupdating the scrollview
-						//very nice :: http://animate.javascript.codesolution.site/520463-3633-animate-animate-wrong-using-track-index-repeat.html
-						c[0].alertObjects.unshift(angular.copy(c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex])
-							);
+						console.log(objectsToSpliceFromBottom);
+						console.log(copiedIndex);
+						console.log(c[0].alertObjects.length);
+						if (objectsToSpliceFromBottom > 1 && !paused) {
+							for (i = 0; i < objectsToSpliceFromBottom; i++) {
+								c[0].alertObjects[i]["displayed"] = !paused;
+							}
+						};
+						if (!paused) {
+							//console.log("came");
+							c[0].alertObjects[c[0].alertObjects.length - objectsToSpliceFromBottom - copiedIndex]["displayed"] = !paused;
+							// c[0].alertObjects.unshift(
+							// 	{
+							// 		id:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].id,
+							// 		data:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].data
+							// 	}
+							// 	);
+							//hack against deep copying since it was notupdating the scrollview
+							//very nice :: http://animate.javascript.codesolution.site/520463-3633-animate-animate-wrong-using-track-index-repeat.html
+							c[0].alertObjects.unshift(angular.copy(c[0].alertObjects[c[0].alertObjects.length - objectsToSpliceFromBottom - copiedIndex]));
 						copiedIndex++;
+						}
+						if (paused) {
+							c[0].alertObjects[c[0].alertObjects.length - objectsToSpliceFromBottom - copiedIndex]["displayed"] =paused;
+							c[0].alertObjects.unshift(angular.copy(c[0].alertObjects[c[0].alertObjects.length - objectsToSpliceFromBottom - copiedIndex]));
+							
+							c[0].alertObjects[0]["displayed"] = false;
+							console.log(c[0].alertObjects[0].displayed);
+							console.log(c[0].alertObjects[0].id);
+						copiedIndex++;
+						}
+						//copiedIndex++;
 						if (c[0].alertObjects.length >= a.count) {
 							copiedIndex--;
-							if(!paused){
-							c[0].alertObjects.splice(c[0].alertObjects.length - 1, 1);
-						};
+							if (!paused) {
+									//copiedIndex--;
+								//c[0].alertObjects.splice(c[0].alertObjects.length - 1, 1);
+								c[0].alertObjects = c[0].alertObjects.slice(0, c[0].alertObjects.length - objectsToSpliceFromBottom);
+								objectsToSpliceFromBottom = 1;
+							} if(paused) {
+								objectsToSpliceFromBottom++;
+								console.log("came");
+							}
 						}
 					}
 				})();
-				c[0].pauseScrollForDemo=function(pause){
-					if(pause){
-						console.log(c[0].alertObjects);
-					$interval.cancel(c[0].timer);
-					c[0].timer=c[0].interval(function(){c[0].addElementForDemo(true)}, a.interval);
-			}else{$interval.cancel(c[0].timer);
-				c[0].timer=c[0].interval(function(){c[0].addElementForDemo(false)}, a.interval);
-			}
+				c[0].pauseScrollForDemo = function(pause) {
+					if (pause) {
+						//console.log(c[0].alertObjects);
+						$interval.cancel(c[0].timer);
+						c[0].timer = c[0].interval(function() {
+							c[0].addElementForDemo(true)
+						}, a.interval);
+					} else {
+						$interval.cancel(c[0].timer);
+						c[0].timer = c[0].interval(function() {
+							c[0].addElementForDemo(false)
+						}, a.interval);
+					}
 
 				};
-				c[0].timer=c[0].interval(function(){c[0].addElementForDemo(false)}, a.interval);
+				c[0].timer = c[0].interval(function() {
+					c[0].addElementForDemo(false)
+				}, a.interval);
 			}
 		}
 	}]);
