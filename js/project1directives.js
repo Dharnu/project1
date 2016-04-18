23c2fab565bc6ebf9b62f02df9f763987af3e261
Hemant <HemantP@muralie.e2info.com>
2016-04-18 13:30:59 +0530

    removed track by index and implemented shallow copy to fix reload bug on view

--- js/project1directives.js
+++ js/project1directives.js
@@ -1,7 +1,18 @@
 (function() {
 	var app = angular.module('project1-directives', ['ngAnimate']);
 
-
+	app.filter('stopdisplay',[function(){
+		return function(alerts){
+			var toDisplayAlerts=[];
+		//	console.log(alerts);
+			for(i=0;i<alerts.length;i++){
+			//	console.log(alerts[i].displayed);
+			if(alerts[i].displayed)
+				toDisplayAlerts.push(alerts[i]);
+		};
+			return toDisplayAlerts;
+		}
+	}]);
 	app.directive('emergencyList', ['communication', '$timeout', '$interval', function(communication, $timeout, $interval) {
 		return {
 			restrict: 'EA',
@@ -15,14 +26,14 @@
 			controller: function($rootScope, $scope) {
 				//this.alertObjects=$scope.$parent.$parent.main.emergencyListItems;				
 				//this.addElement=function
-
+				this.interval=$interval;
 				this.click = function(num) {
 					communication.tellgraph(this.alertObjects[num].id);
 				};
 				this.addEvent = function() {
 					console.log("add event");
 					this.alertObjects.unshift({
-						id: 10,
+						id: 100,
 						data: $scope.createEvent
 					});
 				};
@@ -31,19 +42,47 @@
 			link: function(s, e, a, c) {
 				console.log(a);
 				c[0].alertObjects = s.$parent.$parent.main.emergencyListItems;
+				for(i=0;i<c[0].alertObjects.length;i++){
+					//console.log(c[0].alertObjects[i]);
+					c[0].alertObjects[i]["displayed"]=true;
+				//	c[0].alertObjects[i]["uniqueId"]=new Date().getTime();
+
+				};
 				console.log(c[0].alertObjects);
 				c[0].addElementForDemo = (function() {
 					var copiedIndex = 0;
-					return function() {
-						c[0].alertObjects.unshift(c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex]);
+					return function(paused) {
+						c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex]["displayed"]=!paused;
+						// c[0].alertObjects.unshift(
+						// 	{
+						// 		id:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].id,
+						// 		data:c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex].data
+						// 	}
+						// 	);
+						//hack against deep copying since it was notupdating the scrollview
+						//very nice :: http://animate.javascript.codesolution.site/520463-3633-animate-animate-wrong-using-track-index-repeat.html
+						c[0].alertObjects.unshift(angular.copy(c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex])
+							);
 						copiedIndex++;
 						if (c[0].alertObjects.length >= a.count) {
 							copiedIndex--;
+							if(!paused){
 							c[0].alertObjects.splice(c[0].alertObjects.length - 1, 1);
+						};
 						}
 					}
 				})();
-				$interval(c[0].addElementForDemo, a.interval);
+				c[0].pauseScrollForDemo=function(pause){
+					if(pause){
+						console.log(c[0].alertObjects);
+					$interval.cancel(c[0].timer);
+					c[0].timer=c[0].interval(function(){c[0].addElementForDemo(true)}, a.interval);
+			}else{$interval.cancel(c[0].timer);
+				c[0].timer=c[0].interval(function(){c[0].addElementForDemo(false)}, a.interval);
+			}
+
+				};
+				c[0].timer=c[0].interval(function(){c[0].addElementForDemo(false)}, a.interval);
 			}
 		}
 	}]);
