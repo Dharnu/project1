44ece1e611278170228214def530ae4e532eadb6
Hemant <HemantP@muralie.e2info.com>
2016-04-18 10:29:11 +0530

    delay and number of items setting added to html

--- js/project1directives.js
+++ js/project1directives.js
@@ -2,16 +2,20 @@
 	var app = angular.module('project1-directives', ['ngAnimate']);
 
 
-	app.directive('emergencyList', ['communication','$timeout','$interval', function(communication,$timeout,$interval) {
+	app.directive('emergencyList', ['communication', '$timeout', '$interval', function(communication, $timeout, $interval) {
 		return {
-			restrict: 'E',
-			scope: {},
+			restrict: 'EA',
+			scope: {
+				count: "@",
+				interval: "@"
+			},
+			replace: true,
 			templateUrl: "./partials/emergency-list.html",
 			require: ['emergencyList', '^pageComponents'],
 			controller: function($rootScope, $scope) {
 				//this.alertObjects=$scope.$parent.$parent.main.emergencyListItems;				
 				//this.addElement=function
-				
+
 				this.click = function(num) {
 					communication.tellgraph(this.alertObjects[num].id);
 				};
@@ -25,18 +29,21 @@
 			},
 			controllerAs: 'emergencyList',
 			link: function(s, e, a, c) {
+				console.log(a);
 				c[0].alertObjects = s.$parent.$parent.main.emergencyListItems;
-				
-				c[0].addElement=function(){
-					console.log(c[0].alertObjects.length);
-					c[0].alertObjects.unshift(c[0].alertObjects[c[0].alertObjects.length-1]);
-										console.log(c[0].alertObjects.length);
-
-					// c[0].alertObjects.splice(c[0].alertObjects.length-1,1);
-					// 					console.log(c[0].alertObjects.length);
-
-				};	
-				 $interval(c[0].addElement,5000);
+				console.log(c[0].alertObjects);
+				c[0].addElementForDemo = (function() {
+					var copiedIndex = 0;
+					return function() {
+						c[0].alertObjects.unshift(c[0].alertObjects[c[0].alertObjects.length - 1 - copiedIndex]);
+						copiedIndex++;
+						if (c[0].alertObjects.length >= a.count) {
+							copiedIndex--;
+							c[0].alertObjects.splice(c[0].alertObjects.length - 1, 1);
+						}
+					}
+				})();
+				$interval(c[0].addElementForDemo, a.interval);
 			}
 		}
 	}]);
@@ -68,13 +75,19 @@
 				c[0].graphs = s.$parent.$parent.main.graphImages;
 				c[0].displayImage = c[0].graphs.id1;
 				s.$on("graph", function() {
-					console.log("recieved broadcast");
+					//console.log("recieved broadcast");
 					//console.log(communication.id);
 					var idxx = "id" + communication.id;
-					console.log(idxx);
+					//	console.log(idxx);
 					//console.log(c[0].graphs);
 					//console.log(c[0].graphs[idxx]);
-					c[0].displayImage = c[0].graphs[idxx];
+					if (typeof c[0].graphs[idxx] != "undefined") {
+						//console.log( typeof c[0].graphs[idxx]);}
+						c[0].displayImage = c[0].graphs[idxx];
+					} else {
+						console.log("undefined");
+						c[0].displayImage = "undefined";
+					}
 
 				});
 			}
@@ -90,7 +103,7 @@
 			controller: function() {
 				this.individualValue;
 				this.click = function(num) {
-					console.log("mainview click");
+					//console.log("mainview click");
 					communication.tellgraph(this.individualValue[num].id);
 				};
 			},
